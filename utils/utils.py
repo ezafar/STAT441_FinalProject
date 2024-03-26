a37431269961a2b8c87375b821ac7a75db802bcb
SrijanChaudhuri <76275146+SrijanChaudhuri@users.noreply.github.com>
2024-03-26 15:50:52 -0400

    adding stacking

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def get_training():
-    X = pd.read_csv("./data/X_train.csv", index_col=False)
-    y = pd.read_csv("./data/y_train.csv", index_col=False)
+    X = pd.read_csv("../data/X_train.csv", usecols=lambda column: column != 'id')
+    y = pd.read_csv("../data/y_train.csv", usecols=lambda column: column != 'id')
     X = X.select_dtypes(exclude=['object'])
     return X, y
