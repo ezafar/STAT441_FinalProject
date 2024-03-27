3ee351cf42cc48c9b4d85c993b24c912bfda1532
Zafar Erkinboev <67560303+ezafar@users.noreply.github.com>
2024-03-27 03:56:34 -0400

    additions

--- utils/utils.py
+++ utils/utils.py
@@ -13,14 +13,17 @@ def get_test():
     X_test = pd.read_csv('../data/X_test.csv', index_col=0)
     return X_test
 
-def get_predictions(X_test, model, name, proba=True):
+def get_predictions(X_test, model, proba=True, name=None):
     if proba:
         y_pred = model.predict_proba(X_test)
     else:
         y_pred = model.predict(X_test)
     preds_df = pd.DataFrame(y_pred, columns=['no answer', 'very important', 'quite important', 'not important', 'not at all important'])
     preds_df.index.name = 'id'
-    preds_df.to_csv(f"../data/predictions/{name}.csv")
+    if name is not None:
+        preds_df.to_csv(f"../data/predictions/{name}.csv")
     return preds_df
     
 
+def drop_objects(X):
+    return X.select_dtypes(exclude=['object'])
